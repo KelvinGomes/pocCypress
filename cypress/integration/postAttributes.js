a2c7dd9e81defe238e20050409ef8cf941184765
Kelvin Gomes <kelvin.souza@sensedia.com>
2022-04-11 17:02:52 -0300

    Template

--- cypress/integration/postAttributes.js
+++ cypress/integration/postAttributes.js
@@ -1,17 +1,18 @@
 ///<reference types = "cypress"/>
 import postFixture from '../fixtures/postFixture.json'
+import authentication from "../support/authentication"
 
 describe('PostAttributes', function () {
-    it('Create', function () {
+    it('Create', async function () {
+        const token = await authentication.getToken(Cypress.env('user'), Cypress.env('password'));
         postFixture.name = "Teste-" + Math.floor(Math.random() * 1000);
         cy.request({
             method: 'POST',
             url: '/api-governance/api/v3/attributes/',
             headers: {
-                'Sensedia-Auth': '118b9de3-a0ac-4c66-bea6-fc2be3ecd6eb'
+                'Sensedia-Auth': token
             },
             body: postFixture
-            //failOnStatusCode: false
         }).as('response')
         cy.get('@response').then(res => {
             expect(res.status).to.be.equal(201)
---
+++ cypress/support/authentication.js
@@ -0,0 +1,20 @@
+class Authentication {
+    async getToken(login, password) {
+        const res = await cy.request({
+            method: 'POST',
+            url: 'api-manager/api/v3/login',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: {
+                login,
+                password,
+            },
+        });
+        expect(res.status).to.be.equal(201);
+        const { code } = res.body;
+        const token = JSON.stringify(code);
+        return token;
+    }
+}
+
+module.exports = new Authentication;
+
