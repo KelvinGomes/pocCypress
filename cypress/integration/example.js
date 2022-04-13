c94162ff7a4bf493314b3ff61ead100cd7d78b90
Vitor Marambaia <vitormarambaia@gmail.com>
2022-04-13 10:50:05 -0300

    adjust code for lint

--- cypress/integration/example.js
+++ cypress/integration/example.js
@@ -1,27 +1,26 @@
-///<reference types = "cypress"/>
-import exampleFactory from '../factory/example.factory'
-import authFactory from '../factory/auth.factory'
-import randomUtils from '../utils/random.utils'
+/// <reference types = "cypress"/>
+import exampleFactory from '../factory/example.factory';
+import authFactory from '../factory/auth.factory';
+import randomUtils from '../utils/random.utils';
 
 before(() => {
-    cy.getToken(Cypress.env('user'), Cypress.env('password')).then(token => {
-        authFactory.token = token;
-    })
-})
+  cy.getToken(Cypress.env('user'), Cypress.env('password')).then((token) => {
+    authFactory.token = token;
+  });
+});
 
 describe('Example', () => {
-    it('Create Attribute', () => {
-        exampleFactory.name = randomUtils.getName;
-        cy.request({
-            method: 'POST',
-            url: '/api-governance/api/v3/attributes/',
-            headers: {
-                'Sensedia-Auth': authFactory.token
-            },
-            body: exampleFactory
-        }).then(res => {
-            expect(res.status).to.be.equal(201);
-        })
-    })
-})
-
+  it('Create Attribute', () => {
+    exampleFactory.name = randomUtils.getName;
+    cy.request({
+      method: 'POST',
+      url: '/api-governance/api/v3/attributes/',
+      headers: {
+        'Sensedia-Auth': authFactory.token,
+      },
+      body: exampleFactory,
+    }).then((res) => {
+      expect(res.status).to.be.equal(201);
+    });
+  });
+});
